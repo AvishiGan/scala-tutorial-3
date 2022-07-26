078a41a1a77f5999ab1968e6d460bfa3af72f30a
Avishi Gan <avishiganepola@gmail.com>
2022-07-26 07:18:04 +0530

    Function name changed to 'celsiusToFahrenheit'

--- Scala Tutorial 3/Q2.scala
+++ Scala Tutorial 3/Q2.scala
@@ -1,4 +1,4 @@
 object Q2 extends App{
-    def celciusToFahrenheit(c: Double): Double = c*1.8 + 32.0
-    println(celciusToFahrenheit(35))
+    def celsiusToFahrenheit(c: Double): Double = c*1.8 + 32.0
+    println(celsiusToFahrenheit(35))
 }
